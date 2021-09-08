0ec61d4e9e80e7c5e068286429999fe95aea0e9c
Shaan Faydh <shaan1010faydh@gmail.com>
2021-09-09 00:49:16 +0530

    add license

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,18 @@
+/*-------------------------------------------------------------------------------------------------------
+* Copyright © 2020, Shaan Faydh
+*  
+* The Phoenix Project is free software licensed under GPL v3.0.
+* You can redistribute and/or modify it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+* 
+* The Phoenix Project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with The Phoenix Project.  If not, see <http://www.gnu.org/licenses/>.
+---------------------------------------------------------------------------------------------------------*/
+
 // @dart=2.10
 import 'package:phoenix/src/Begin/utilities/constants.dart';
 import 'package:phoenix/src/Begin/pages/settings/settings_pages/privacy.dart';
