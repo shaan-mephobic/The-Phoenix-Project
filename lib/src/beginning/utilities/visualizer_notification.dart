06934cba98bde39aad2126456522d40b8f9c6c57
Shaan Faydh <shaan1010faydh@gmail.com>
2021-11-05 16:33:45 +0530

    update packages

--- lib/src/beginning/utilities/visualizer_notification.dart
+++ lib/src/beginning/utilities/visualizer_notification.dart
@@ -2,28 +2,32 @@ import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 
 visualizerNotificationInit() async {
-  await AwesomeNotifications()
-      .initialize('resource://drawable/phoenix_awaken', [
-    NotificationChannel(
-      channelKey: 'phoenix_visualize',
-      channelName: 'Phoenix Visualizer',
-      channelDescription: 'Phoenix Visualizer Running Alert',
-      enableLights: false,
-      enableVibration: false,
-      importance: NotificationImportance.Default,
-      playSound: false,
-    )
-  ]);
+  await AwesomeNotifications().initialize(
+    'resource://drawable/phoenix_awaken',
+    [
+      NotificationChannel(
+        channelKey: 'phoenix_visualize',
+        channelName: 'Phoenix Visualizer',
+        channelDescription: 'Phoenix Visualizer Running Alert',
+        enableLights: false,
+        enableVibration: false,
+        importance: NotificationImportance.Default,
+        playSound: false,
+      ),
+    ],
+  );
 }
 
 startVisualizerNotification() async {
-  AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
-    if (!isAllowed) {
-      // Insert here your friendly dialog box before call the request method
-      // This is very important to not harm the user experience
-      AwesomeNotifications().requestPermissionToSendNotifications();
-    }
-  });
+  AwesomeNotifications().isNotificationAllowed().then(
+    (isAllowed) {
+      if (!isAllowed) {
+        // Insert here your friendly dialog box before call the request method
+        // This is very important to not harm the user experience
+        AwesomeNotifications().requestPermissionToSendNotifications();
+      }
+    },
+  );
   await AwesomeNotifications().createNotification(
     content: NotificationContent(
       id: 69,
@@ -31,7 +35,6 @@ startVisualizerNotification() async {
       locked: true,
       title: 'Phoenix Visualizing Music',
       displayedLifeCycle: NotificationLifeCycle.AppKilled,
-      autoCancel: false,
       body: 'Running in background',
       backgroundColor: Colors.black,
     ),
